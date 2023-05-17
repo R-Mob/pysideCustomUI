b579b3dfef22afe2faff2db661fad6a3f9874133
R-Mob <rupam.pro9@gmail.com>
2023-05-17 21:39:47 +0530

    Initial setup

--- pysideCustomUI/UIconfig.py
+++ pysideCustomUI/UIconfig.py
@@ -1,4 +1,21 @@
 #This is a UI setup file using pyside2
+from PySide2 import QtCore
+from PySide2 import QtWidgets
+from shiboken2 import wrapInstance
 
-import maya.cmds as cmds
-cmds.polySphere()
+import maya.OpenMayaUI as omui
+
+
+def maya_main_window():
+    main_window_ptr = omui.MQtUtil.mainWindow()
+    return wrapInstance(int(main_window_ptr),QtWidgets.QWidget)
+
+class TestDialog(QtWidgets.QDialog):
+
+    def __int__(self,parent = maya_main_window()):
+        super(TestDialog,self).__init__(parent)
+
+
+if __name__ == "__main__":
+    d = TestDialog()
+    d.show()
